5879b1759a36f547bb0fd85a33c98f1a9fc4d3b5
Shivam Garg <41623220+sgshivamgarg8@users.noreply.github.com>
2020-10-01 20:53:02 +0530

    Update C++ Program to represent table of any number using for loop.cpp
    Shortened code by adding macro

--- C++ Program to represent table of any number using for loop.cpp
+++ C++ Program to represent table of any number using for loop.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
+#define FOR(i, n) for (int i = 1; i <= n; i++)
 
-int main ()
-{
+int main () {
     int a;
     cout << "Enter the number: ";
     cin >> a;
-    for (int i = 1; i <= 10; i++)
-    {
+    // for (int i = 1; i <= 10; i++)
+    FOR(i, 10) {
         cout << (i * a) << endl;
     }
-    return 0;
+    // return 0;
 }
